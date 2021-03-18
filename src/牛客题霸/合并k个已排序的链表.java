709bd883a3a81a2e838a700144d4a114b97a8c92
xieli <2524546110@qq.com>
2021-03-18 22:01:06 +0800

    牛客题霸

---
+++ src/牛客题霸/合并k个已排序的链表.java
@@ -0,0 +1,77 @@
+package 牛客题霸;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * Created by xieli on 2021/3/18.
+ */
+public class 合并k个已排序的链表 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    /**
+     * https://leetcode-cn.com/problems/merge-k-sorted-lists/solution/4-chong-fang-fa-xiang-jie-bi-xu-miao-dong-by-sweet/
+     *
+     * @param lists
+     * @return
+     */
+
+    // 对 K 条链表进行两两合并
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode res = null;
+        for (ListNode list : lists) {
+            res = mergeTwoLists(res, list);
+        }
+        return res;
+    }
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode tmp = new ListNode(0);
+        ListNode cur = tmp;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 == null ? l2 : l1;
+        return tmp.next;
+    }
+
+    public ListNode mergeKLists_02(ListNode[] lists) {
+        // 默认小根堆, 必须写出来, 否则编译通不过
+        Queue<ListNode> pq = new PriorityQueue<>((v1, v2) -> v1.val - v2.val);
+        for (ListNode node : lists) {
+            if (node != null) {
+                pq.offer(node);
+            }
+        }
+
+        ListNode dummyHead = new ListNode(-1);
+        ListNode tail = dummyHead;
+        while (!pq.isEmpty()) {
+            ListNode minNode = pq.poll();
+            tail.next = minNode;
+            tail = minNode;
+            if (minNode.next != null) {
+                pq.offer(minNode.next);
+            }
+        }
+
+        return dummyHead.next;
+    }
+
+}
--- src/牛客题霸/合并有序链表.java
+++ src/牛客题霸/合并有序链表.java
@@ -14,6 +14,7 @@ public class 合并有序链表 {
         }
     }
 
+    // 迭代
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode tmp = new ListNode(0);
         ListNode cur = tmp;
@@ -29,6 +30,23 @@ public class 合并有序链表 {
         }
         cur.next = l1 == null ? l2 : l1;
         return tmp.next;
+    }
+
+    // 递归
+    public ListNode mergeTwoLists_02(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
     }
 
 }
