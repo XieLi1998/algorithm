9031bf5047f2d558db5b8079c65fc444ff0d6fe5
xieli <2524546110@qq.com>
2021-03-21 15:27:08 +0800

    leetcode

--- src/leetcode/_647_回文子串.java
+++ src/leetcode/_647_回文子串.java
@@ -6,7 +6,26 @@ package leetcode;
 public class _647_回文子串 {
 
     public int countSubstrings(String s) {
-        return 0;
+        int res = 0;
+
+        // 注意，此题必须取到最后一位，因为此题并不是求最长回文子串，而是问有多少个回文子串
+        for (int i = 0; i < s.length(); i++) {
+            res += palindrome(s, i, i);
+            res += palindrome(s, i, i + 1);
+        }
+        return res;
+    }
+
+    public int palindrome(String s, int left, int right) {
+        int count = 0;
+
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+            count++;
+        }
+
+        return count;
     }
 
 }
